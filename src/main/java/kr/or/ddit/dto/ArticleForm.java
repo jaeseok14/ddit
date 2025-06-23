3c8847a3eb9e0ecc8722e98f667bd4e2b6751a14
jaeseok14 <akslaktt@gmail.com>
2025-06-23 20:12:34 +0900

    드디어!!!

--- src/main/java/kr/or/ddit/dto/ArticleForm.java
+++ src/main/java/kr/or/ddit/dto/ArticleForm.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 // dto는 vo의 역할을 하는 클래스를 모아둘 수 있는 영역입니다.
-// 해당 클래스가 데이터를 받아 올 그릇이 됩니다.
+// 해당 클래스가 데이터를 받아 올 그릇이 됩니다. 드디어!
 
 // @NoArgsConstructor 어노테이션은 ArticleForm 클래스에 선언된 필드를 사용하지 않고 기본 생성자를 생성할 때 사용합니다.
 // @Getter와 @Setter 어노테이션은 dto 클래스의 getter/setter 메서드를 만들 때 사용합니다.
