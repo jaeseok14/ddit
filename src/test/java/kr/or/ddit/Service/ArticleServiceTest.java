ddaea46afd26ffd867c4d1d5c3c4ad0d927ef9d8
jaeseok14 <akslaktt@gmail.com>
2025-06-24 18:05:23 +0900

    변경함

--- src/test/java/kr/or/ddit/Service/ArticleServiceTest.java
+++ src/test/java/kr/or/ddit/Service/ArticleServiceTest.java
@@ -17,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class ArticleServiceTest {
 
+
     // # 테스트
     // 테스트는 프로그램의 품질을 검증하는 것으로, 의도대로 프로그램이 잘 동작하는지 확인하는 과정입니다.
     // 테스트 초창기에는 사람이 직접 요청을 보내고 응답을 받아 일일이 확인하는 방식으로 진행하였습니다.
